e9d3d94daf68241e48ebee4bc582818f1e846333
tomas <tomas.maraschio@gmail.com>
2021-08-29 00:24:33 -0300

    add bhaskara resolution

--- main.cpp
+++ main.cpp
@@ -1,21 +1,58 @@
 #include <iostream>
 #include <cmath>
 
+
 double getDiscriminant(double a, double b, double c)
 {
 	return pow(b, 2) - (4 * a * c);
 }
 
+void calcAndPrintImaginary(double a, double b, double discriminant)
+{
+	double realPart{ (-b) / (2 * a) };
+	double imaginaryPart{ sqrt(-discriminant) / (2 * a) };
+	std::cout << "Solution 1: " << realPart << " + "
+		<< imaginaryPart << "i\n";
+
+	std::cout << "Solution 2: " << realPart << " - "
+		<< imaginaryPart << "i\n";
+}
+
+void calcAndPrintReal(double a, double b, double discriminant)
+{
+	double solution1{ (-b + sqrt(discriminant)) / (2 * a) };
+	double solution2{ (-b - sqrt(discriminant)) / (2 * a) };
+
+	std::cout << "Solution 1: " << solution1 << '\n';
+
+	if (solution1 != solution2)
+		std::cout << "Solution 2: " << solution2 << '\n';
+}
+
+void calcAndPrintBhaskara(double a, double b, double c)
+{
+	double discriminant{ getDiscriminant(a, b, c) };
+
+	if (discriminant < 0)
+	{
+		calcAndPrintImaginary(a, b, discriminant);
+	}
+	else
+	{
+		calcAndPrintReal(a, b, discriminant);
+	}
+}
+
 int main()
 {
-	std::cout << "a = 1, b = 4, c = 2\n";
-	std::cout << "discriminant: " << getDiscriminant(1.0, 4.0, 2.0) << '\n';
+	std::cout << "a = 2, b = 6, c = 4\n";
+	calcAndPrintBhaskara(2.0, 6.0, 4.0);
 
-	std::cout << "a = 4, b = -7, c = 21\n";
-	std::cout << "discriminant: " << getDiscriminant(4.0, -7.0, 21.0) << '\n';
+	std::cout << "a = 1, b = 2, c = 1\n";
+	calcAndPrintBhaskara(1.0, 2.0, 1.0);
 
-	std::cout << "a = 1.23, b = -7, c = 21\n";
-	std::cout << "discriminant: " << getDiscriminant(1.23, -7.0, 21.0) << '\n';
+	std::cout << "a = 2, b = 3, c = 4\n";
+	calcAndPrintBhaskara(2.0, 3.0, 4.0);
 
 	return 0;
 }
